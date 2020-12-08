c57ae322d1aa4b418393e1699e44a258247e3921
AHmims <ali.hmims99@gmail.com>
2020-12-08 16:29:20 +0100

    signature

--- Brief_03/src/com/company/Main.java
+++ Brief_03/src/com/company/Main.java
@@ -3,6 +3,8 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
+        //ALI HMIMS
+        //
         Personne[] personnes = new Personne[2];
         personnes[0] = new Medecin("Maher", 31);
         personnes[1] = new Opticienne("Iman", 33);
