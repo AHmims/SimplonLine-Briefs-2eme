9be2d6f518459dbb2182774c8ca2b9d865015c7b
AHmims <ali.hmims99@gmail.com>
2020-12-22 15:20:29 +0100

    init

---
+++ Brief_06/src/sample/InterListe.java
@@ -0,0 +1,8 @@
+package sample;
+
+import java.util.ArrayList;
+
+public interface InterListe {
+    public ArrayList<Quiz> listeQuiz = new ArrayList<Quiz>();
+    public ArrayList<Player_QUIZ> listePlayer_QUIZ = new ArrayList<Player_QUIZ>();
+}
