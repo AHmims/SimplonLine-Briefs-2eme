2316c614a6ccb48c347164a3b4d3bc683ca36ff7
AHmims <ali.hmims99@gmail.com>
2021-01-07 17:34:06 +0100

    make the login window responsive

--- Brief_07/src/sample/Main.java
+++ Brief_07/src/sample/Main.java
@@ -13,7 +13,7 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
         primaryStage.setTitle("SimplonLine");
-        primaryStage.setScene(new Scene(root, 657, 420));
+        primaryStage.setScene(new Scene(root));
         //primaryStage.getIcons().add(new Image(getClass().getResourceAsStream("res/favicon.ico")));
         primaryStage.getIcons().add(new Image(getClass().getResourceAsStream("res/favicon.png")));
         primaryStage.setResizable(false);
