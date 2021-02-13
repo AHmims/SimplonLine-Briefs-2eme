9dc2328703e611285ee9cbd9e7eaf8166d4f35ba
AHmims <ali.hmims99@gmail.com>
2021-02-13 12:56:51 +0100

    This story takes place in an unknown place, an unknown time period of a an anonymous person

---
+++ Brief_10/tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      fontFamily: {
+        'Kollektif': ['Kollektif', 'sans-serif'],
+        'Source Sans Pro': ['Source Sans Pro', 'sans-serif']
+      }
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
