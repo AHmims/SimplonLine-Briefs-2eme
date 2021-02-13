c488c0ab89480b76737b1cd702255ab39dbbc196
AHmims <ali.hmims99@gmail.com>
2021-02-13 21:52:18 +0100

    As soon as I opened my eyes a cool gust of wind came charging to my face. .....Where am I

--- Brief_10/web/js/articles.js
+++ Brief_10/web/js/articles.js
@@ -1,8 +1,28 @@
 const __articles = document.getElementsByClassName('articleCard');
-const __articles_btn = document.getElementsByClassName('voteBtn');
-// 
+//
+let __last_selected_article = -1;
+//
 for (let i = 0; i < __articles.length; i++) {
     __articles[i].addEventListener('click', e => {
-        console.log(e.target);
+        if (e.target.dataset.display == undefined) {
+            //side bar
+            const sideBar = document.getElementById('sideBar');
+            //side bar management
+            if (__last_selected_article != __articles[i].dataset.article) {
+                //update content
+
+                //show if not shown
+                if (sideBar.classList.contains('hidden'))
+                    sideBar.classList.remove('hidden');
+                // 
+                __last_selected_article = __articles[i].dataset.article;
+            } else {
+                //hide side bar
+                if (!sideBar.classList.contains('hidden'))
+                    sideBar.classList.add('hidden');
+                // 
+                __last_selected_article = -1;
+            }
+        }
     });
 }
--- Brief_10/web/js/articles_CLIENT.js
+++ Brief_10/web/js/articles_CLIENT.js
@@ -12,9 +12,17 @@ for (let i = 0; i < articles_btn.length; i++) {
                 //DUMP BUT GOOD FOR FUTURE CHANGES
                 let state = articles_btn[i].dataset.toggle;
                 if (state == 'true') {
-                    articles_btn[i].innerText = "VOTE";
+                    // articles_btn[i].innerText = "VOTE";
+                    articles_btn[i].classList.add("bg-coolGray-100");
+                    articles_btn[i].classList.remove("bg-red-50");
+                    articles_btn[i].children[0].classList.add("text-coolGray-300");
+                    articles_btn[i].children[0].classList.remove("text-red-500");
                 } else {
-                    articles_btn[i].innerText = "unVOTE";
+                    // articles_btn[i].innerText = "unVOTE";
+                    articles_btn[i].classList.remove("bg-coolGray-100");
+                    articles_btn[i].classList.add("bg-red-50");
+                    articles_btn[i].children[0].classList.remove("text-coolGray-300");
+                    articles_btn[i].children[0].classList.add("text-red-500");
                 }
                 articles_btn[i].dataset.toggle = state == 'true' ? false : true;
             } else if (response.data.status == 0) {
