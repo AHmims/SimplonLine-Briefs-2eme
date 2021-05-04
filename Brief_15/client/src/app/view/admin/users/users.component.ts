12a3be023be2361130062170d2805ac61571c618
AHmims <ali.hmims99@gmail.com>
2021-05-04 15:39:52 +0000

    display list of users

--- Brief_15/client/src/app/view/admin/users/users.component.ts
+++ Brief_15/client/src/app/view/admin/users/users.component.ts
@@ -1,5 +1,7 @@
 import {Component, ComponentFactoryResolver, OnInit, ViewChild, ViewContainerRef} from '@angular/core';
 import {UtilisateurService} from '../../../services/utilisateur/utilisateur.service';
+import {Utilisateur} from '../../../models/utilisateur.model';
+import {TableUsersComponent} from '../../../components/table/table-users/table-users.component';
 
 @Component({
   selector: 'app-users',
@@ -8,13 +10,22 @@ import {UtilisateurService} from '../../../services/utilisateur/utilisateur.serv
 })
 export class UsersComponent implements OnInit {
 
-  @ViewChild('usersHolder', {read: ViewContainerRef}) prodsHolder!: ViewContainerRef;
+  @ViewChild('usersHolder', {read: ViewContainerRef}) usersHolder!: ViewContainerRef;
 
   constructor(private utilisateurService: UtilisateurService, private componentFactoryResolver: ComponentFactoryResolver) {
   }
 
   ngOnInit(): void {
-    this.utilisateurService.getAll().subscribe(data => console.log(data), error => console.error(error));
+    this.utilisateurService.getAll().subscribe(data => {
+      console.log(data);
+      this.displayUsers(data);
+    }, error => console.error(error));
   }
 
+  displayUsers = (utilisateurs: Utilisateur[]) => {
+    this.usersHolder.clear();
+    const componentFactory = this.componentFactoryResolver.resolveComponentFactory(TableUsersComponent);
+    const dynamicComponent = <TableUsersComponent> this.usersHolder.createComponent(componentFactory).instance;
+    dynamicComponent.utilisateurs = utilisateurs;
+  };
 }
