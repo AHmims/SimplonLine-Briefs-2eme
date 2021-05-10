30b55864ba6dffff4c602d4ae8a295d772394612
AHmims <ali.hmims99@gmail.com>
2021-05-10 15:28:59 +0000

    updated coupon model

--- Brief_15/src/main/java/ahmims/BasmaOnlineStore/model/Coupon.java
+++ Brief_15/src/main/java/ahmims/BasmaOnlineStore/model/Coupon.java
@@ -17,9 +17,14 @@ public class Coupon {
     private String idCoupon;
     @Column(name = "codeCoupon")
     private String codeCoupon;
+    @Column(name = "remiseCoupon")
+    private double remiseCoupon;
     @Column(name = "dateCreation")
     @Temporal(TemporalType.TIMESTAMP)
     private Date dateCreation;
+    @Column(name = "dateFin")
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date dateFin;
     @Column(name = "valide")
     private boolean valide;
     //
@@ -31,27 +36,31 @@ public class Coupon {
     //
     //
 
-    public Coupon(String idCoupon, String codeCoupon, Date dateCreation, boolean valide, List<Commande> commandes, Administrateur administrateur) {
+    public Coupon(String idCoupon, String codeCoupon, double remiseCoupon, Date dateCreation, Date dateFin, boolean valide, List<Commande> commandes, Administrateur administrateur) {
         this.idCoupon = idCoupon;
         this.codeCoupon = codeCoupon;
+        this.remiseCoupon = remiseCoupon;
         this.dateCreation = dateCreation;
+        this.dateFin = dateFin;
         this.valide = valide;
         this.commandes = commandes;
         this.administrateur = administrateur;
     }
 
-    public Coupon(String codeCoupon, Date dateCreation, boolean valide, Administrateur administrateur) {
+    public Coupon(String codeCoupon, double remiseCoupon, Date dateCreation, Date dateFin, boolean valide, Administrateur administrateur) {
         this.codeCoupon = codeCoupon;
+        this.remiseCoupon = remiseCoupon;
         this.dateCreation = dateCreation;
+        this.dateFin = dateFin;
         this.valide = valide;
         this.administrateur = administrateur;
     }
 
     public Coupon() {
     }
+    //
+    //
 
-    //
-    //
     public String getIdCoupon() {
         return idCoupon;
     }
@@ -68,12 +77,28 @@ public class Coupon {
         this.codeCoupon = codeCoupon;
     }
 
+    public double getRemiseCoupon() {
+        return remiseCoupon;
+    }
+
+    public void setRemiseCoupon(double remiseCoupon) {
+        this.remiseCoupon = remiseCoupon;
+    }
+
     public Date getDateCreation() {
         return dateCreation;
     }
 
     public void setDateCreation(Date dateCreation) {
         this.dateCreation = dateCreation;
+    }
+
+    public Date getDateFin() {
+        return dateFin;
+    }
+
+    public void setDateFin(Date dateFin) {
+        this.dateFin = dateFin;
     }
 
     public boolean isValide() {
